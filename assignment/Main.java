867d8605d7a28194ff6060d9ff31e4324a17e876
dimathlon <115566173+dimathlon@users.noreply.github.com>
2022-11-03 12:18:21 +0000

    made 2 songs

--- assignment/Main.java
+++ assignment/Main.java
@@ -6,11 +6,17 @@ public class Main {
     {
         System.out.println("Dime's CW1 submission");
 
-        Song done = new Song("What I've done", "Linkin Park");
+        Song whatIveDone = new Song("What I've done", "Linkin Park", 44512512);
 
-        done.print();
+        whatIveDone.print();
+        
+        Song soul = new Song ("Sell Your Soul", "Hollywood Undead", 125152);
 
+        soul.print();
         
 
-    }
-}
+    }    Song both = new Song (title: "Both", name: "Headie One", plays 142345);
+
+         both.print();
+
+}       
--- assignment/Song.java
+++ assignment/Song.java
@@ -2,15 +2,19 @@ public class Song
 {
     private String name;
     private String title;
+    private int plays;
 
-    public Song(String title, String name)
+    public Song(String title, String name, int plays)
     {
         this.title = title;
         this.name = name;
+        this.plays= plays;
     }
 
     public void print()
     {
-        System.out.println("Title: " + this.title + " Name: " + this.name);
+        System.out.println("Title: " + this.title + "\t Name: " + this.name + " \t plays:" + this.plays);
     }
+
+    
 }
